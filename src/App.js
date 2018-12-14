0d947e70f48db3c652c139c7da94cb2a101817dc
Christopher O'Grady <cfogrady@gmail.com>
2018-12-13 23:19:53 -0500

    Working Modal. Initial testing of FileSystem APIs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Modal from './components/testModal';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showModal: false};
+    this.hideModal = this.hideModal.bind(this);
+    this.showModal = this.showModal.bind(this);
+  }
+
+  hideModal() {
+    this.setState({showModal: false});
+  }
+
+  showModal() {
+    this.setState({showModal: true});
+  }
+
+  testDirectory = () => {
+    alert('test');
+    // requires actually making an app and testing differently than a web page...
+    // maybe use sandboxed file storage, with zip transfer mechanism...
+    //chrome.fileSystem.chooseEntry({type: 'openDirectory'}, this.fileHandleCallback);
+  };
+
+  fileHandleCallback = (entry, fileEntries, properties) => {
+    alert('got it!');
+  };
+
   render() {
     return (
       <div className="App">
@@ -19,6 +47,12 @@ class App extends Component {
           >
             Learn React
           </a>
+          <button type="button" onClick={this.showModal}>Open Modal</button>
+          <Modal show={this.state.showModal} handleClose={this.hideModal}>
+            <p>Modal</p>
+            <p>Data</p>
+            <button type="button" onClick={this.testDirectory}>OpenDirectory</button>
+          </Modal>
         </header>
       </div>
     );
